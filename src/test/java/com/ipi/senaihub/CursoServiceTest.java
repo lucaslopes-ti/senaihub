676a334a4f8559773df6d994297ee1b9380d7db0
Ana-Clara3 <anaclara08ssantos@gmail.com.br>
2026-05-26 09:28:45 -0300

    LARISSA - Teste totalVagas 0.

--- src/test/java/com/ipi/senaihub/CursoServiceTest.java
+++ src/test/java/com/ipi/senaihub/CursoServiceTest.java
@@ -3,12 +3,15 @@ package com.ipi.senaihub;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Assertions;
 import com.ipi.senaihub.model.Curso;
 import com.ipi.senaihub.repository.CursoRepository;
+import com.ipi.senaihub.service.CursoService;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class) // Anotação para habilitar o suporte ao Mockito no JUnit 5, permitindo o uso de objetos falsos e injeção de dependências durante os testes.
@@ -35,6 +38,20 @@ public class CursoServiceTest {
         Assertions.assertEquals("O número total de vagas não pode ser negativo ou zero.", exception.getMessage()); // Verifica se a mensagem da exceção lançada é a esperada, confirmando que a validação está funcionando corretamente.
 
         Mockito.verify(cursoRepository, Mockito.never()).save(Mockito.any(Curso.class)); // Verifica se o método save do cursoRepository nunca foi chamado, garantindo que o curso não foi salvo no banco de dados devido à validação falha.
-    
+    }
+    //LARISSA: Crie o teste que valida a regra de negócio quando totalVagas é zero.
+    @Test
+    public void cursoInvalido(){
+        Curso cursoInvalido = new Curso();
+        cursoInvalido.setNome ("IPI");
+        cursoInvalido.setTotalVagas(0);
+
+        IllegalArgumentException excecao = Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> cursoService.salvarCurso(cursoInvalido)
+        );
+        Assertions.assertEquals("O total de vagas deve ser maior que zero.", excecao.getMessage());
+
+        Mockito.verify(cursoRepository, Mockito.never()).save(Mockito.any(Curso.class));
     }
 }
